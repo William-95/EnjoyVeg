847e29f40c50b54628f3424f05bbc930183a3ed7
William-95 <verga.william.95@gmail.com>
2022-12-07 16:02:09 +0100

    a

---
+++ src/pages/Error.js
@@ -0,0 +1,10 @@
+import React from "react";
+import errorImg from "../img/error-404.png";
+
+export default function Error() {
+  return (
+    <div>
+      <img src={errorImg} className="featureIcon" alt="" />
+    </div>
+  );
+}
